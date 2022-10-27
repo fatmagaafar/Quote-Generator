c1f1173e565d1ae5947d192bab005caeb1519232
fatmagaafar <48574600+fatmagaafar@users.noreply.github.com>
2022-10-27 03:30:44 +0200

    third update

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const text=document.getElementById("quote");
 const author=document.getElementById("author");
-const tweetButton=document.getElementById("tweet");
+const btn=document.getElementById("btn");
+
 
 const getNewQuote = async () =>
 {
     //api for quotes
-    var url="https://type.fit/api/quotes";    
+    let API="https://type.fit/api/quotes";    
 
     // fetch the data from api
-    const response=await fetch(url);
+    const response=await fetch(API);
     console.log(typeof response);
     //convert response to json and store it in quotes array
     const allQuotes = await response.json();
 
     // Generates a random number between 0 and the length of the quotes array
-    const indx = Math.floor(Math.random()*allQuotes.length);
+    const index = Math.floor(Math.random()*allQuotes.length);
 
     //Store the quote present at the randomly generated index
-    const quote=allQuotes[indx].text;
+    const quote=allQuotes[index].text;
     
     //Store the author of the respective quote
-    const auth=allQuotes[indx].author;
+    const auth=allQuotes[index].author;
 
     if(auth==null)
     {
@@ -31,9 +32,6 @@ const getNewQuote = async () =>
     text.innerHTML=quote;
     author.innerHTML="~ "+auth;
 
-    //tweet the quote
-    tweetButton.href="https://twitter.com/intent/tweet?text="+quote+" ~ "+auth;
-
 }
 
-getNewQuote();
+btn.addEventListener('click',getNewQuote);
